a577e5b01dca7e7c87d19adaa74cdafe32915849
RuntyConjurer <169057857+RuntyConjurer@users.noreply.github.com>
2025-02-23 13:10:56 -0400

    Add files via upload

---
+++ CalculadoraTemperaturas/CalculadoraTemperaturas/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CalculadoraTemperaturas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Centigrados = new System.Windows.Forms.TextBox();
+            this.Fahrenheit = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Grados Centigrados";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Grados Fahrenheit";
+            // 
+            // Centigrados
+            // 
+            this.Centigrados.Location = new System.Drawing.Point(247, 45);
+            this.Centigrados.Name = "Centigrados";
+            this.Centigrados.Size = new System.Drawing.Size(213, 20);
+            this.Centigrados.TabIndex = 2;
+            this.Centigrados.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Fahrenheit
+            // 
+            this.Fahrenheit.Location = new System.Drawing.Point(247, 91);
+            this.Fahrenheit.Name = "Fahrenheit";
+            this.Fahrenheit.Size = new System.Drawing.Size(213, 20);
+            this.Fahrenheit.TabIndex = 3;
+            this.Fahrenheit.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 178);
+            this.Controls.Add(this.Fahrenheit);
+            this.Controls.Add(this.Centigrados);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Centigrados;
+        private System.Windows.Forms.TextBox Fahrenheit;
+    }
+}
+
---
+++ CalculadoraTemperaturas/CalculadoraTemperaturas/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraTemperaturas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            if (Centigrados.Text == "")
+            {
+                Fahrenheit.Clear();
+                return;
+            }
+
+            if (double.TryParse(Centigrados.Text, out double celsius))
+            {
+                double fahrenheit = (celsius * 9 / 5) + 32;
+               Fahrenheit.Text = fahrenheit.ToString("0.##");
+            }
+            else
+            {
+                Fahrenheit.Clear();
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (Fahrenheit.Text == "")
+            {
+                Centigrados.Clear();
+                return;
+            }
+
+            if (double.TryParse(Fahrenheit.Text, out double fahrenheit))
+            {
+                double celsius = (fahrenheit - 32) * 5 / 9;
+                Centigrados.Text = celsius.ToString("0.##");
+            }
+            else
+            {
+                Centigrados.Clear();
+            }
+        }
+    }
+}
